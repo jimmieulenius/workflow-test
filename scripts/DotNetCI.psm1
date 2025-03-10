7b57ebcd27d4d5a795718a190379289e2263844c
Jimmie Ulenius <jimmie.ulenius@eon.com>
2025-03-10 01:03:26 +0100

    wip

--- scripts/DotNetCI.psm1
+++ scripts/DotNetCI.psm1
@@ -11,20 +11,27 @@ function Test-ProjectAssets {
         $Prefix
     )
 
-    $contentObject = Get-Content `
-        -Path "$Path/obj/project.assets.json" `
-        -Raw `
-    | ConvertFrom-Json `
-        -AsHashtable
+    if (-not $AllowPrerelease) {
+        $contentObject = Get-Content `
+            -Path "$Path/obj/project.assets.json" `
+            -Raw `
+        | ConvertFrom-Json `
+            -AsHashtable
 
-    if ($contentObject.targets) {
-        $contentObject.targets.GetEnumerator() `
-        | ForEach-Object {
-            $version = $_.Key -split '/' `
-            | Select-Object `
-                -Last 1
+        if ($contentObject.targets) {
+            $contentObject.targets.GetEnumerator() `
+            | ForEach-Object {
+                if ($_.Value) {
+                    $_.Value.GetEnumerator() `
+                    | ForEach-Object {
+                        $version = $_.Key -split '/' `
+                        | Select-Object `
+                            -Last 1
 
-            $version
+                        $version
+                    }
+                }
+            }
         }
     }
 }
