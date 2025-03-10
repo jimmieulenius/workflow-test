15bee3cba1c2caa56370f9f48f3023104b078120
Jimmie Ulenius <jimmie.ulenius@eon.com>
2025-03-10 02:09:27 +0100

    wip

--- src/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ src/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0234f7fe007167242c81854649e9c61ff0d9ff98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d05fa5cc6c767e14338a18ba8dab7740bbaf93af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
