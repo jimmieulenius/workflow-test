d2137a74440a6c83655212494bb133b214a60d14
Jimmie Ulenius <jimmie.ulenius@eon.com>
2025-03-13 00:53:57 +0100

    wip

--- src/ClassLibrary1/obj/Debug/net8.0/ClassLibrary1.AssemblyInfo.cs
+++ src/ClassLibrary1/obj/Debug/net8.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f8a50ec26714b667a5943af8bbb0bb8e082990")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d3924c1b9d850fb12cf0bea7d812353d1a21b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ src/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed6edbdda90f77f2b14fb2449e33a1ad5f16e48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d3924c1b9d850fb12cf0bea7d812353d1a21b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
